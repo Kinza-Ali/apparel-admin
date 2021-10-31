88a44b74726f63a1a0382c72bc5d0cd6dcc8ea04
Kinza-Alii <kinza.ali@retailo.co>
2021-11-01 00:35:11 +0500

    topbarWrapper name fixed with import topbar css file

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import "./Topbar.css";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 function Topbar() {
   return (
     <div className="topbar">
-      <div className="topbar">
+      <div className="topbarWrapper">
         <div className="topleft">
           <span className="logo">Soul</span>
         </div>
