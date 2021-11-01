facbdc1c4860ad72c519c5709a848865aae2c547
Kinza-Alii <kinza.ali@retailo.co>
2021-11-01 05:57:19 +0500

    React-router-dom implemented

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import "./App.css";
 import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
+import Home from "./components/pages/home/Home";
+import Products from "./components/pages/products/Products";
+import Orders from "./components/pages/order/Orders";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Users from "./components/pages/user/Users";
+import UpdateUser from "./components/pages/user/UpdateUser";
+import NewUser from "./components/pages/user/NewUser";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <div className="others"> Other Pages</div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/users">
+            <Users />
+          </Route>
+          <Route path="/users/:userId">
+            <UpdateUser />
+          </Route>
+          <Route path="/newUser">
+            <NewUser />
+          </Route>
+          <Route path="/products">
+            <Products />
+          </Route>
+          <Route path="/orders">
+            <Orders />
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
