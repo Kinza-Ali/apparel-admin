a21f375ec82430fd99590d8a02e6784feaf1adb6
Kinza-Alii <kinza.ali@retailo.co>
2021-11-26 14:16:46 +0500

    New Order Validations Completed

--- src/components/pages/order/NewOrder.jsx
+++ src/components/pages/order/NewOrder.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import AddIcon from "@mui/icons-material/Add";
@@ -6,259 +6,228 @@ import RemoveOutlinedIcon from "@mui/icons-material/RemoveOutlined";
 import "./NewOrder.css";
 import { addOrder } from "../../redux/actions/orderActions";
 import { getProductById } from "../../redux/actions/productActions";
-import { Formik, Form, Field, FieldArray, ErrorMessage } from "formik";
-import * as Yup from "yup";
-import { TextField } from "@material-ui/core";
 
 function NewOrder() {
   const dispatch = useDispatch();
   const history = useHistory();
-  const initialValues = {
-    customerId: "",
-    deliveryDate: "",
-    itemList: [{ productId: "", quantity: "" }],
-  };
 
   // let productIdError = "";
-  // productIdError = useSelector((state) => state.allProducts.error);
-  // const [deliveryDate, setDeliveryDate] = useState();
-  // const [customerId, setCustomerId] = useState();
-  // // const [itemList, setItemList] = useState([{ productId: "", quantity: "" }]);
-  // const [errorMessage, setErrorMessage] = useState([
-  //   { errorProductId: "required", errorQuantity: "required" },
-  // ]);
-  // const [dateError, setDateError] = useState();
-  // const order = [
-  //   {
-  //     label: "Customer Id",
-  //     type: "text",
-  //     placeholder: 34454,
-  //   },
-  //   {
-  //     label: "Delivery Date",
-  //     type: "text",
-  //     placeholder: "2021-03-03",
-  //   },
-  // ];
+  const productIdError = useSelector((state) => state.allProducts.error);
+  const [deliveryDate, setDeliveryDate] = useState();
+  const [customerId, setCustomerId] = useState();
+  const [address, setAddress] = useState();
+  const [itemList, setItemList] = useState([{ productId: "", quantity: "" }]);
+  // const errors = [];
+  const [errorMessage, setErrorMessage] = useState([
+    { errorProductId: "", errorQuantity: "" },
+  ]);
+  const [dateError, setDateError] = useState("");
+  const [id, setId] = useState();
+  const order = [
+    {
+      label: "Customer Id",
+      type: "text",
+      placeholder: 34454,
+    },
+    {
+      label: "Address",
+      type: "text",
+      placeholder: "Xyz - Street",
+    },
+    {
+      label: "Delivery Date",
+      type: "text",
+      placeholder: "2021-03-03",
+    },
+  ];
 
-  // // handle click event of the Add button
+  useEffect(() => {
+    dispatch(getProductById(id));
+    // eslint-disable-next-line
+  }, [id]);
 
-  // const handleInput = (e, label) => {
-  //   if (label === "Customer Id") {
-  //     setCustomerId(e.target.value);
-  //   } else if (label === "Delivery Date") {
-  //     setDeliveryDate(e.target.value);
-  //   }
-  // };
-
-  // const handleInputChange = (e, index) => {
-  //   const { name, value } = e.target;
-  //   const list = [...itemList];
-  //   list[index][name] = value;
-  //   setItemList(list);
-  // };
-
-  // // handle click event of the Remove button
-  // const handleRemoveClick = (index) => {
-  //   const list = [...itemList];
-  //   list.splice(index, 1);
-  //   setItemList(list);
-  // };
-
-  // const renderInputComponent = (order) => {
-  //   return (
-  //     <input
-  //       type={order.type}
-  //       placeholder={order.placeholder}
-  //       onChange={(e) => handleInput(e, order.label)}
-  //     />
-  //   );
-  // }
-
-  // const handleAddClick = (e, index) => {
-  //   e.preventDefault();
-
-  //   // dispatch(getProductById(itemList[index].productId));
-
-  //   console.log("Starting again " + " index is : " + index);
-  //   let errorMsg = [];
-  //   let error = {};
-  //   let valid = true;
-  //   console.log(itemList[index].productId);
-  //   if (itemList[index].productId) {
-  //     console.log("indside: " + itemList[index].productId);
-  //     dispatch(getProductById(itemList[index].productId));
-  //     console.log(productIdError);
-  //     if (productIdError) {
-  //       console.log("inner if");
-  //       error.errorProductId = "Enter a valid product Id";
-  //       console.log(error.errorProductId + " checking if the value is set");
-  //       valid = false;
-  //     } else {
-  //       console.log("valid product");
-  //       error.errorProductId = "";
-  //     }
-  //   } else if (!itemList[index].productId) {
-  //     console.log("no product id");
-  //     error.errorProductId = "Enter Product Id";
-  //     valid = false;
-  //   } else {
-  //     console.log("in else prod id");
-  //     error.errorProductId = "";
-  //   }
-  //   if (!itemList[index].quantity) {
-  //     console.log("Quantity is empty");
-  //     error.errorQuantity = "Quantity is required";
-  //     valid = false;
-  //   } else if (itemList[index].quantity < 1) {
-  //     console.log("quantity is less than 1");
-  //     error.errorQuantity = "Quantity must be greater than 0";
-  //     valid = false;
-  //   } else {
-  //     error.errorQuantity = " ";
-  //   }
-  //   if (valid) {
-  //     setItemList([...itemList, { productId: "", quantity: "" }]);
-  //   }
-
-  //   errorMsg = error;
-
-  //   if (index === 0) {
-  //     setErrorMessage([errorMsg]);
-  //   } else {
-  //     setErrorMessage([...errorMessage, errorMsg]);
-  //   }
-
-  //   console.log(JSON.stringify(errorMsg));
-  //   console.log(JSON.stringify(errorMessage));
-  // };
-
-  // const handleAddOrder = () => {
-  //   let dateRegx = /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/;
-  //   let dateValidation = dateRegx.test(deliveryDate);
-  //   console.log(dateValidation);
-  //   if (!dateValidation) {
-  //     setDateError("Enter date in YYYY-MM-DD format");
-  //   } else {
-  //     const orderList = {
-  //       item: itemList,
-  //       customerId,
-  //       deliveryDate,
-  //     };
-  //     dispatch(addOrder(orderList));
-  //     history.push({ pathname: "/orders" });
-  //   }
-  // };
-  //Formik
-
-  const validationSchema = Yup.object().shape({
-    customerId: Yup.string().required("Required"),
-    deliveryDate: Yup.date().required("Required"),
-    itemList: Yup.array().of(
-      Yup.object().shape({
-        productId: Yup.string().min(4, "too short").required("Required"), // these constraints take precedence
-        quantity: Yup.string()
-          .min(1, "Must be greater than 0")
-          .required("Required"), // these constraints take precedence
-      })
-    ),
-    //where to add custom validations
-
-    // .required('Must have friends') // these constraints are shown if and only if inner constraints are satisfied
-    // .min(3, 'Minimum of 3 friends'),
-  });
-  const onSubmit = (values, props) => {
-    // console.log(values.email, values.password);
-    // dispatch(loginUser(values.email, values.password));
-    console.log(values);
-    // dispatch(addOrder(values));
-    //     history.push({ pathname: "/orders" });
+  const handleInput = (e, label) => {
+    if (label === "Customer Id") {
+      setCustomerId(e.target.value);
+    } else if (label === "Delivery Date") {
+      setDeliveryDate(e.target.value);
+    } else {
+      setAddress(e.target.value);
+    }
   };
+
+  const handleInputChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...itemList];
+    list[index][name] = value;
+    setItemList(list);
+    if (name === "productId") {
+      setId(value);
+      console.log(id);
+    }
+  };
+
+  // handle click event of the Remove button
+  const handleRemoveClick = (index) => {
+    const list = [...itemList];
+    list.splice(index, 1);
+    setItemList(list);
+  };
+
+  const renderInputComponent = (order) => {
+    return (
+      <input
+        type={order.type}
+        placeholder={order.placeholder}
+        onChange={(e) => handleInput(e, order.label)}
+      />
+    );
+  };
+
+  const handleAddClick = (e, index) => {
+    e.preventDefault();
+
+    let errorMsg = [];
+    let error = {};
+    let valid = true;
+    if (itemList[index].productId) {
+      if (productIdError) {
+        error.errorProductId = "Enter a valid product Id";
+        valid = false;
+      } else {
+        error.errorProductId = "";
+      }
+    } else if (!itemList[index].productId) {
+      error.errorProductId = "Enter Product Id";
+      valid = false;
+    } else {
+      error.errorProductId = "";
+    }
+    if (!itemList[index].quantity) {
+      error.errorQuantity = "Quantity is required";
+      valid = false;
+    } else if (itemList[index].quantity < 1) {
+      error.errorQuantity = "Quantity must be greater than 0";
+      valid = false;
+    } else {
+      error.errorQuantity = "";
+    }
+    if (valid) {
+      setItemList([...itemList, { productId: "", quantity: "" }]);
+    }
+
+    errorMsg = error;
+    if (index === 0) {
+      setErrorMessage([errorMsg]);
+    } else if (Object.keys(errorMessage).length > index) {
+      errorMessage[index].errorProductId = errorMsg.errorProductId;
+      errorMessage[index].errorQuantity = errorMsg.errorQuantity;
+    } else {
+      setErrorMessage([
+        ...errorMessage,
+        {
+          errorProductId: errorMsg.errorProductId,
+          errorQuantity: errorMsg.errorQuantity,
+        },
+      ]);
+    }
+  };
+
+  const handleAddOrder = (e) => {
+    e.preventDefault();
+    // eslint-disable-next-line
+    let dateRegx = /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/;
+    let dateValidation = dateRegx.test(deliveryDate);
+    if (!dateValidation) {
+      setDateError("Enter date in YYYY-MM-DD format");
+    } else {
+      const orderList = {
+        item: itemList,
+        customerId,
+        deliveryDate,
+        address,
+      };
+      dispatch(addOrder(orderList));
+      history.push({ pathname: "/orders" });
+    }
+  };
+
   return (
     <div className="newOrder">
       <h1 className="addOrderTitle">New Order</h1>
-      <Formik
-        initialValues={initialValues}
-        onSubmit={onSubmit}
-        validationSchema={validationSchema}
-      >
-        {(props) => (
-          <Form>
-            <div>
-              <FieldArray
-                name="itemList"
-                render={(arrayHelpers) => (
-                  <div>
-                    {initialValues.itemList.map((item, index) => (
-                      <div key={index}>
-                        {/** both these conventions do the same */}
-                        <Field name={`itemList[${index}].productId`} />
-                        <Field name={`itemList.${index}.quantity`} />
+      <form className="addOrderForm">
+        {itemList.map((item, index) => {
+          return (
+            <div key={index} className="addOrder">
+              <div>
+                <label>Product Id</label>
+                <div>
+                  <input
+                    name="productId"
+                    placeholder="657890456"
+                    value={item.productId}
+                    onChange={(e) => handleInputChange(e, index)}
+                  />
 
-                        <button
-                          type="button"
-                          onClick={() => arrayHelpers.remove(index)}
-                        >
-                          -
-                        </button>
-                      </div>
-                    ))}
-                    <button
-                      type="button"
-                      onClick={() =>
-                        arrayHelpers.push({ productId: "", quantity: "" })
-                      }
-                    >
-                      +
-                    </button>
-                  </div>
+                  <span style={{ color: "red" }} key={index}>
+                    {errorMessage.length - 1 >= index &&
+                      errorMessage[index].errorProductId}
+                  </span>
+                </div>
+              </div>
+              <div>
+                <label> Quantity </label>
+                <div>
+                  <input
+                    className="ml10"
+                    name="quantity"
+                    placeholder="23"
+                    value={item.quantity}
+                    onChange={(e) => handleInputChange(e, index)}
+                  />
+                </div>
+                <span style={{ color: "red" }} key={index}>
+                  {errorMessage.length - 1 >= index &&
+                    errorMessage[index].errorQuantity}
+                </span>
+              </div>
+              <div className="addOrder">
+                {itemList.length !== 1 && (
+                  <button
+                    className="mr10"
+                    onClick={() => handleRemoveClick(index)}
+                  >
+                    <RemoveOutlinedIcon />
+                  </button>
                 )}
-              />
-              <div>
-                <Field
-                  as={TextField}
-                  name="customerId"
-                  placeholder="Enter customer id"
-                  type="text"
-                  fullWidth
-                  required
-                  helperText={<ErrorMessage name="customerId" />}
-                  className="my-1 p-4"
-                />
-              </div>
-              <div>
-                <Field
-                  as={TextField}
-                  name="deliveryDate"
-                  placeholder="Enter date"
-                  type="text"
-                  fullWidth
-                  required
-                  helperText={
-                    <ErrorMessage
-                      name="deliveryDate"
-                      style={{ color: "red" }}
-                    />
-                  }
-                  className="my-1 p-4"
-                />
-              </div>
-              <div>
-                <button
-                  type="submit"
-                  color="primary"
-                  variant="contained"
-                  disabled={props.isSubmitting}
-                  fullWidth
-                  className="btn"
-                >
-                  {props.isSubmitting ? "Loading" : "Add Order"}
-                </button>
+                {itemList.length - 1 === index && (
+                  <button
+                    className="ml10"
+                    onClick={(e) => handleAddClick(e, index)}
+                  >
+                    <AddIcon />
+                  </button>
+                )}
               </div>
             </div>
-          </Form>
-        )}
-      </Formik>
+          );
+        })}
+
+        {order.map((order, index) => {
+          return (
+            <div className="addOrderItem" key={index}>
+              <label>{order.label}</label>
+              {renderInputComponent(order)}
+            </div>
+          );
+        })}
+        <p style={{ color: "red", paddingRight: 20, marginTop: 20 }}>
+          {dateError}
+        </p>
+        <button className="addOrderButton" onClick={(e) => handleAddOrder(e)}>
+          Add Order
+        </button>
+      </form>
     </div>
   );
 }
