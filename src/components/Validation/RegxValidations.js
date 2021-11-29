e7b346ced32cdc0f3e73c1b0bec4f773ccaa4105
Kinza-Alii <kinza.ali@retailo.co>
2021-11-29 14:14:43 +0500

    Contact validation updated

--- src/components/Validation/RegxValidations.js
+++ src/components/Validation/RegxValidations.js
@@ -1,3 +1,3 @@
-export const phoneRegx = /^\d{14}$/;
+export const phoneRegx = /^\d{11}$/;
 export const nameRegx = /^(?!\s)([a-z ,.'-]+)$/;
 export const emailRegx = /^[\w.%+-]+@[\w.-]+\.[\w]{2,6}$/;
