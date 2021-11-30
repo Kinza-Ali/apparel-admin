dee0abb1aeeaa5cc61caca9ba30222a86211ff23
Kinza-Alii <kinza.ali@retailo.co>
2021-11-30 18:02:29 +0500

    Url changed in Services.js

--- src/services/Services.js
+++ src/services/Services.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000/api/";
+axios.defaults.baseURL =
+  "http://ec2-3-250-68-254.eu-west-1.compute.amazonaws.com:3000/api/";
 axios.defaults.headers.common["x-auth-token"] = localStorage.getItem("token");
 class Services {
   get = (url) =>
